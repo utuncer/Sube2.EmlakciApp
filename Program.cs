a7ab960eb355eeaf39b614e3c2e4888ec010577c
Umut Tunçer <utuncerx@gmail.com>
2025-02-26 10:02:07 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sube2.EmlakciApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
